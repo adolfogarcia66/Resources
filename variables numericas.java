87d39b64ca705a6862c271c510ded6a1508c6ef0
MiguelVega10 <mickyvega1410@gmail.com>
2024-12-18 12:42:11 -0600

    Create variables numericas.java

---
+++ variables numericas.java
@@ -0,0 +1,14 @@
+public class variables constantes {
+	public static void main (String[]){
+	int a= 3;
+	int b= 2;
+	a=a+b;
+	System.out.println( "EL valor de a es" +(a+b));//5
+	System.out.println("El valor de a es" +(a-b));//1	
+	System.out.println("El valor de a es"+(a*b));//6
+	System.out.println("El valor de a es"+(a/b));//1.5
+	System.out.println("El valor de a es "+(a%b));//1
+	}
+}
+
+
